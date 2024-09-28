f96cec240f3dce42ae93f20fcf18ecbc2f27c4fe
Sam06tanb <78392140+sam06tanb@users.noreply.github.com>
2024-09-28 03:49:15 -0300

    MySQL Connection
    Connection link between JAVA and SQL Workbench 8.0

---
+++ src/test/java/com/example/projetosigmagames4/ProjetoSigmaGames4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetosigmagames4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSigmaGames4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
